97b7970ab47468eb22ff0c0eb801ad97c67d7fa2
pooya.oa <pooya.oladazimi@tib.eu>
2021-11-08 10:49:00 +0100

    overwrite the original template view

---
+++ ckanext/dataset_metadata_automation/libs.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+
+class Helper():
+
+    def get_user_name():
+
+        return "Pooya"
--- ckanext/dataset_metadata_automation/plugin.py
+++ ckanext/dataset_metadata_automation/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+from ckanext.dataset_metadata_automation.libs import Helper
 
 
 class DatasetMetadataAutomationPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +13,7 @@ class DatasetMetadataAutomationPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         
+
+    def get_helpers(self):
+
+        return {'get_user_name': Helper.get_user_name}
